2f1665b7f58282a0f7e428d4d1d06f58751e0bd3
shankarkasala <umashankarno66@gmail.com>
2021-12-27 14:53:26 +0530

    commit

---
+++ ConcatenationofArray.js
@@ -0,0 +1,10 @@
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+
+var getConcatenation = function (nums) {
+  var arr = [];
+  return (arr = nums.concat(nums));
+};
