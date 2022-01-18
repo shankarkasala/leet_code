e880f660fff2640f6ccc33fff099cc249c5c54a5
shankarkasala <umashankarno66@gmail.com>
2022-01-18 16:59:13 +0530

    commit

---
+++ Easy/Ordered Stream.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ */
+var OrderedStream = function (n) {
+  this.array = new Array(n);
+  this.p = 0;
+};
+
+/**
+ * @param {number} idKey
+ * @param {string} value
+ * @return {string[]}
+ */
+OrderedStream.prototype.insert = function (idKey, value) {
+  this.array[idKey - 1] = value;
+  let res = [];
+  while (this.array[this.p]) {
+    res.push(this.array[this.p++]);
+  }
+  return res;
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */
+
+// Input
+// ["OrderedStream", "insert", "insert", "insert", "insert", "insert"]
+// [[5], [3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]]
+// Output
+// [null, [], ["aaaaa"], ["bbbbb", "ccccc"], [], ["ddddd", "eeeee"]]
+
+// Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
+// OrderedStream os = new OrderedStream(5);
+// os.insert(3, "ccccc"); // Inserts (3, "ccccc"), returns [].
+// os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
+// os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
+// os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
+// os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+// Concatentating all the chunks returned:
+// [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
+// The resulting order is the same as the order above.
