0b72ac9908f7e5ac9cb87258c7399e03c5209607
shankarkasala <umashankarno66@gmail.com>
2022-02-24 16:38:01 +0530

    commit

---
+++ Strings/Easy/Final Value of Variable.js
@@ -0,0 +1,14 @@
+var finalValueAfterOperations = function (operations) {
+  return operations.reduce(
+    (acc, curr) => (curr.includes("++") ? ++acc : --acc),
+    0
+  );
+};
+
+// Input: operations = ["--X","X++","X++"]
+// Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
